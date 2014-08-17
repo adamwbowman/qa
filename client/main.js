12383578b2d1f86786ee24df2b08d0496707b0d8
adamwbowman <adamwbowman@gmail.com>
2014-08-17 17:36:32 -0500

    - putting emailing on hold

--- client/main.js
+++ client/main.js
@@ -398,7 +398,7 @@ var notifyCommentors = function (itemId) {
 	var answersAuthors = Answers.find({_id: itemId}).fetch();
 	console.log(questionAuthor);
 	console.log(answersAuthors);
-	sendEmail();
+	//sendEmail();
 };
 var sendEmail = function () {
 	Email.send({
